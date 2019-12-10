020c06dbaedf8e460ee84f249cee4679fcfcf652
wjp241 <woojae.jay.park@gmail.com>
2019-12-10 16:58:35 +0900

    delete javaexam

--- javaexam/src/day4/Hello.java
+++
@@ -1,7 +0,0 @@
-package day4;
-
-public class Hello {
-	public void bye() {
-		System.out.print("Bye!");
-	}
-}
--- javaexam/src/sketch.java
+++
@@ -1,9 +0,0 @@
-
-public class sketch {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.printf("Hello %s!%n", "World");	
-		}
-
-}
